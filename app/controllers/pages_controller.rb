7769d9987b067050b4accbdc25fe3d420997076a
Dana Jones <dana@sterlingrosedesign.com>
2009-07-03 14:47:33 -0500

    Added most recently-updated topics to home page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @most_recent = WikiPage.find(:all, :limit => 20, :include => :content, :order => 'wiki_contents.updated_on DESC')
   end
 end
